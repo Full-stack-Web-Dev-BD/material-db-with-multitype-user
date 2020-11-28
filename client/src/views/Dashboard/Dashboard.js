eb0922d0bdf963e786540a565294e8db59be6199
Alamin <sai.amaraneni94@gmail.com>
2020-11-28 11:45:49 +0600

    done

---
+++ client/src/views/Dashboard/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  }
+}));
+
+const Dashboard = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={4}
+      >
+        <h1>Dashboard</h1>
+      </Grid>
+    </div>
+  );
+};
+
+export default Dashboard;
